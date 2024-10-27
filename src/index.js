5ec874273312d31bd90350f41d0d388389520aef
Waltteri <waltteri.westerholm@gmail.com>
2024-10-27 21:54:53 +0200

    done tasks, quote api.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import http from 'http';
-import {getItems, postItem} from './items.js';
+import {getItems, postItem, deleteItem, updateItem} from './items.js';
 const hostname = '127.0.0.1';
 const port = 3000;
 
@@ -12,6 +12,12 @@ const server = http.createServer((req, res) => {
     getItems(res);
   } else if (url === '/items' && method === 'POST') {
     postItem(req, res);
+  } else if (url.startsWith('/items/') && method === 'DELETE') {
+    const id = url.split('/')[2];
+    deleteItem(id, res);
+  } else if (url.startsWith('/items/') && method === 'PUT') {
+    const id = url.split('/')[2];
+    updateItem(id, req, res);
   } else {
     // Generic not found response
     res.writeHead(404, {'Content-Type': 'application/json'});
--- src/items.js
+++ src/items.js
@@ -1,31 +1,59 @@
-// Dummy mock data
-const items = [
-  {id: 1, name: 'Item1'},
-  {id: 2, name: 'Item2'},
+// Data used for api, random famos quotes
+
+const quotes = [
+  { id: 1, text: 'To be or not to be', author: 'Shakespeare' },
+  { id: 2, text: 'I think, therefore I am', author: 'Descartes' },
+  { id: 3, text: 'I have a dream', author: 'Martin Luther King' },
+  { id: 4, text: 'Tonights the night', author: 'Dexter Morgan'}
 ];
 
 const getItems = (res) => {
   res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify(items));
+  res.end(JSON.stringify(quotes));
 };
 
 const postItem = (req, res) => {
-  let body = [];
-  req
-    .on('data', (chunk) => {
-      body.push(chunk);
-    })
-    .on('end', () => {
-      body = Buffer.concat(body).toString();
-      // at this point, `body` has the entire request body stored in it as a string
-      console.log('req body:', body);
-      const item = JSON.parse(body);
-      // TODO: check largest id in array and increment by 1
-      item.id = items.length + 1; // NOT like this
-      items.push(item);
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify({message: 'Item added'}));
-    });
+  let body = '';
+  req.on('data', (chunk) => {
+    body += chunk;
+  });
+  req.on('end', () => {
+    const quote = JSON.parse(body);
+    quotes.push(quote);
+    res.writeHead(201, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(quote));
+  });
 };
 
-export {getItems, postItem};
+const deleteItem = (id, res) => {
+  const index = quotes.findIndex((quote) => quote.id === parseInt(id));
+  if (index > -1) {
+    quotes.splice(index, 1);
+    res.writeHead(204, {'Content-Type': 'application/json'});
+    res.end();
+  } else {
+    res.writeHead(404, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({error: '404', message: 'not found'}));
+  }
+};
+
+const updateItem = (id, req, res) => {
+  let body = '';
+  req.on('data', (chunk) => {
+    body += chunk;
+  });
+  req.on('end', () => {
+    const quote = JSON.parse(body);
+    const index = quotes.findIndex((quote) => quote.id === parseInt(id));
+    if (index > -1) {
+      quotes[index] = quote;
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify(quote));
+    } else {
+      res.writeHead(404, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify({error: '404', message: 'not found'}));
+    }
+  });
+};
+
+export {getItems, postItem, deleteItem, updateItem};
