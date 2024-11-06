83bc9e763e15be14e966ce883091f384a88c343a
waltteriwe <walttewe@metropolia.fi>
2024-11-06 20:39:19 +0200

    assignement completed

--- src/items.js
+++ src/items.js
@@ -1,59 +1,34 @@
-// Data used for api, random famous quotes
-
-const quotes = [
-  {id: 1, text: 'To be or not to be', author: 'Shakespeare'},
-  {id: 2, text: 'I think, therefore I am', author: 'Descartes'},
-  {id: 3, text: 'I have a dream', author: 'Martin Luther King'},
-  {id: 4, text: 'Tonights the night', author: 'Dexter Morgan'},
+// Dummy mock data
+const items = [
+  {id: 1, name: 'Item1'},
+  {id: 2, name: 'Item2'},
+  {id: 5, name: 'Item5'},
 ];
 
 const getItems = (res) => {
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify(quotes));
+  res.json(items);
 };
 
 const postItem = (req, res) => {
-  let body = '';
-  req.on('data', (chunk) => {
-    body += chunk;
-  });
-  req.on('end', () => {
-    const quote = JSON.parse(body);
-    quotes.push(quote);
-    res.writeHead(201, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify(quote));
-  });
+  console.log('post req body', req.body);
+  const newItem = req.body;
+  newItem.id = items[items.length - 1].id + 1;
+  items.push(newItem);
+  res.status(201).json({message: 'Item added', id: newItem.id});
 };
 
-const deleteItem = (id, res) => {
-  const index = quotes.findIndex((quote) => quote.id === parseInt(id));
-  if (index > -1) {
-    quotes.splice(index, 1);
-    res.writeHead(204, {'Content-Type': 'application/json'});
-    res.end();
+const getItemById = (req, res) => {
+  const id = parseInt(req.params.id);
+  const item = items.find((item) => item.id === id);
+  if (item) {
+    if (req.query.format === 'plain') {
+      res.send(item.name);
+    } else {
+      res.json(item);
+    }
   } else {
-    res.writeHead(404, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({error: '404', message: 'not found'}));
+    res.status(404).json({message: 'Item not found'});
   }
 };
 
-const updateItem = (id, req, res) => {
-  let body = '';
-  req.on('data', (chunk) => {
-    body += chunk;
-  });
-  req.on('end', () => {
-    const quote = JSON.parse(body);
-    const index = quotes.findIndex((quote) => quote.id === parseInt(id));
-    if (index > -1) {
-      quotes[index] = quote;
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify(quote));
-    } else {
-      res.writeHead(404, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify({error: '404', message: 'not found'}));
-    }
-  });
-};
-
-export {getItems, postItem, deleteItem, updateItem};
+export {getItems, postItem, getItemById};
